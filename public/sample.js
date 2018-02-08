e42e050c2d403bddc3725bba04fc03f1754bf9e6
David Whitlow <davidw@david-whitlow-mbp.local>
2018-02-08 09:18:04 -0800

    atlantis

--- public/sample.js
+++ public/sample.js
@@ -191,7 +191,7 @@
   // This is called after the DOM is loaded, so we can safely bind all the
   // listeners here.
   function initialize() {
-    var center = new google.maps.LatLng(39.48921, -119.79329);
+    var center = new google.maps.LatLng(39.48901, -119.79319);
     var mapOptions = {
       zoom: 20,
       center: center
